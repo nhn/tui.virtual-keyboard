326108a68efe655cb121dffd0404573736791320
icebelle <icebelle@nhnent.com>
2015-02-04 15:36:55 +0900

    기본 필요 파일 커밋

---
+++ src/VirtualKeyboard.js
@@ -0,0 +1,31 @@
+/**
+ * @fileoverview 유저의 키입력 시간 정보를 캡쳐하는 모듈
+ * @author FE개발팀 이제인 <jein.yi@nhnent.com>
+ * @dependency jquery-1.8.3.min.js, common.js
+ */
+
+/* istanbul ignore next */
+if (!ne.component) {
+    ne.component = {};
+}
+
+/**
+ * 클릭 또는 터치로 사용자 입력을 받는 가상키보드 컴포넌트
+ * @example
+ *
+ * // 인스턴스 생성
+ * // 자판 배열을 받아와 가상키보드를 생성한다.
+ * var vkeyboard = new ne.component.VirtualKeyboard({
+ * @todo
+ * });
+ * @constructor ne.component.VirtualKeyboard
+ */
+ne.component.VirtualKeyboard = ne.util.defineClass(/** @lends ne.component.VirtualKeyboard.prototype */{
+    /**
+     * 초기화 함수
+     * @param {object} options 가상키보드를 초기화 옵션
+     */
+    init: function(options) {
+        @todo
+    }
+});
